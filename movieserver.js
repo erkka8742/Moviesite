238d366db0db2ab4bc91ee8363f3efb090c5e365
erkka8742 <erkka.lappala1@elisanet.fi>
2023-07-27 22:19:44 +0300

    nice gg

--- movieserver.js
+++ movieserver.js
@@ -7,6 +7,8 @@ app.use(express.static('public'));
 const fs = require('fs');
 const axios = require('axios');
 
+process.setMaxListeners(20);
+
 // favicon
 app.get('/favicon.ico', (req, res) => {
     res.sendFile(path.resolve(__dirname, './favicon.ico'));
@@ -14,43 +16,42 @@ app.get('/favicon.ico', (req, res) => {
 
 allMatches = [];
 let year = 2023
+content = ""
+url = 'https://www.fandango.com/movies-in-theaters'
+
 
 async function everything() {
 
-// tämä funktio etsii imdbstä leffalistan
-async function scrapeSite(url) {
-
+// etsii fandangosta leffalistan
 while (allMatches.length < 1) {
     console.log("searching...")
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+    let browser = await puppeteer.launch({
+        headless: "new" 
+    });
+    let page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537');
     await page.goto(url, {waitUntil: 'networkidle0'});
 
-    const content = await page.content();  // Get the whole HTML content of the page
+    content = await page.content();  // Get the whole HTML content of the page
 
     // Use regex to find what you need in the HTML content
-    const regex = /"titleText":{"text":"(.*?)"/g;
-    let matches;
-    while ((matches = regex.exec(content)) !== null) {
-        allMatches.push(matches[1]);  // matches[1] contains the captured group
+    const regex = /title" aria-hidden="true">(.*?)\(/g;
+    let match;
+    while ((match = regex.exec(content)) !== null && allMatches.length < 10) {
+        allMatches.push(match[1]);
     }
     await browser.close();
 }
-    
-}
-
-await scrapeSite('https://www.imdb.com/chart/boxoffice/?ref_=hm_cht_sm')
-
 
 re_runtime = ""
 re_director = ""
 re_plot = ""
 re_rating = ""
 re_poster = ""
+re_score = ""
 
-// tämä tehdään jokaiselle elokuvalle
+// tämä tehdään jokaiselle elokuvalle erikseen
 async function findInfo(movie) {
     // etsii tiedot
     try {
@@ -83,9 +84,53 @@ async function findInfo(movie) {
         console.error('Error:', error);
     }
 
+    // etsi elokuvan imdb-url
+    let browser;
+    try {
+        let browser = await puppeteer.launch({
+            headless: "new"
+        });
+        let page = await browser.newPage();
+        let baseUrl = 'https://www.google.com/search';
+        let query = `${movie} IMDb`;
+        let encodedQuery = encodeURIComponent(query);
+        let url2 = `${baseUrl}?q=${encodedQuery}&btnI`;
+        await page.goto(url2);
+        // Get the URL of the resulting page
+        imdbUrl = page.url();
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (browser) {
+            browser.close();
+        }
+    }
+    let regex = /=(.*)/;
+    let match6 = imdbUrl.match(regex);
+    let okImdbUrl = (match6[1]);
+    
+    console.log(okImdbUrl)
+    let browser2 = await puppeteer.launch();
+    let page2 = await browser2.newPage();
 
+    await page2.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537');
+    await page2.goto(okImdbUrl, {waitUntil: 'networkidle0'});
 
-    let results = [re_runtime, re_director, re_plot, re_rating, re_poster];
+    content2 = await page2.content();  // Get the whole HTML content of the page
+    await browser2.close();
+
+    // etsi imdb-score
+    const regex3 = /"ratingValue":\s*([\d.]+)/g;
+    let match3;
+    allMatches2 = [];
+    while ((match3 = regex3.exec(content2)) !== null) {
+        allMatches2.push(match3[1]);
+    }
+    
+    console.log(allMatches2)
+    re_score = allMatches2[1];
+    
+    let results = [re_runtime, re_director, re_plot, re_rating, re_poster, re_score];
     return results;
 }
 
@@ -101,30 +146,31 @@ let movie8info = await findInfo(allMatches[7])
 let movie9info = await findInfo(allMatches[8]) 
 let movie10info = await findInfo(allMatches[9])  
 
-//
+
 class movies {
 
-    constructor(name, runtime, director, plot, rating, poster){
+    constructor(name, runtime, director, plot, rating, poster, score){
         this.name = name
         this.runtime = runtime
         this.director = director
         this.plot = plot
         this.rating = rating
         this.poster = poster
+        this.score = score
     }
 }
 
 // luodaan objectit elokuvista class movies mukaan
-let movie1 = new movies(allMatches[0], movie1info[0], movie1info[1], movie1info[2], movie1info[3], movie1info[4])
-let movie2 = new movies(allMatches[1], movie2info[0], movie2info[1], movie2info[2], movie2info[3], movie2info[4])
-let movie3 = new movies(allMatches[2], movie3info[0], movie3info[1], movie3info[2], movie3info[3], movie3info[4])
-let movie4 = new movies(allMatches[3], movie4info[0], movie4info[1], movie4info[2], movie4info[3], movie4info[4])
-let movie5 = new movies(allMatches[4], movie5info[0], movie5info[1], movie5info[2], movie5info[3], movie5info[4])
-let movie6 = new movies(allMatches[5], movie6info[0], movie6info[1], movie6info[2], movie6info[3], movie6info[4])
-let movie7 = new movies(allMatches[6], movie7info[0], movie7info[1], movie7info[2], movie7info[3], movie7info[4])
-let movie8 = new movies(allMatches[7], movie8info[0], movie8info[1], movie8info[2], movie8info[3], movie8info[4])
-let movie9 = new movies(allMatches[8], movie9info[0], movie9info[1], movie9info[2], movie9info[3], movie9info[4])
-let movie10 = new movies(allMatches[9], movie10info[0], movie10info[1], movie10info[2], movie10info[3], movie10info[4])
+let movie1 = new movies(allMatches[0], movie1info[0], movie1info[1], movie1info[2], movie1info[3], movie1info[4], movie1info[5])
+let movie2 = new movies(allMatches[1], movie2info[0], movie2info[1], movie2info[2], movie2info[3], movie2info[4], movie2info[5])
+let movie3 = new movies(allMatches[2], movie3info[0], movie3info[1], movie3info[2], movie3info[3], movie3info[4], movie3info[5])
+let movie4 = new movies(allMatches[3], movie4info[0], movie4info[1], movie4info[2], movie4info[3], movie4info[4], movie4info[5])
+let movie5 = new movies(allMatches[4], movie5info[0], movie5info[1], movie5info[2], movie5info[3], movie5info[4], movie5info[5])
+let movie6 = new movies(allMatches[5], movie6info[0], movie6info[1], movie6info[2], movie6info[3], movie6info[4], movie6info[5])
+let movie7 = new movies(allMatches[6], movie7info[0], movie7info[1], movie7info[2], movie7info[3], movie7info[4], movie7info[5])
+let movie8 = new movies(allMatches[7], movie8info[0], movie8info[1], movie8info[2], movie8info[3], movie8info[4], movie8info[5])
+let movie9 = new movies(allMatches[8], movie9info[0], movie9info[1], movie9info[2], movie9info[3], movie9info[4], movie9info[5])
+let movie10 = new movies(allMatches[9], movie10info[0], movie10info[1], movie10info[2], movie10info[3], movie10info[4], movie10info[5])
 
 allmovies = [movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10]
 
